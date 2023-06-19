6460e973dc71970381fbeee1684345db84bfb38a
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-06-19 13:30:33 -0500

    Added variables and prompts to JS code

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ if (passwordLength < 8 || passwordLength > 128) {
   alert("Password must be between 8 and 128 chracters!")
   return;
 }
+var includeLowerCase = prompt("Do you want your password to include lowercase characters?");
+
+var includeUpperCase = prompt("Do you want your password to include uppercase characters?");
+
+var includeNumeric = prompt("Do you want your password to include numeric characters?");
+
+var includeSpecial = prompt("Do you want your password to include special characters?");
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
