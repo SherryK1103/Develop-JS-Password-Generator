829a9563b4fcfc3cc0c9911b6b480dc06a7d9d15
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-06-19 14:26:37 -0500

    removed unnecessary code from the way bottom

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var upperCase = lowerCase.toUpperCase();
 var numeric = "0123456789";
 var special = "!@#$%^&*()";
 
-function generatePassword() {
+function generatePassword() {  
 var passwordLength = prompt("How long do you want your password to be?");
 if (passwordLength < 8 || passwordLength > 128) {
   alert("Password must be between 8 and 128 chracters!")
@@ -38,25 +38,17 @@ var generatedPassword = ""
 
 for (let index = 0; index < passwordLength; index++) {
   var characterIndex = getRandomInt(passwordChoice.length);
-  passwordChoice[characterIndex];  
-}
+  generatedPassword = generatedPassword.concat(passwordChoice[characterIndex]);  
+} 
+return generatedPassword;
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function addition(number1,number2) {
-  return number1 + number2;
-}
-
-var result = addition(5, 7)
