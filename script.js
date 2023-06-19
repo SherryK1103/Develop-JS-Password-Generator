012f9194c848fee45863c54a5710e0f9e9bd78da
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-06-19 14:10:50 -0500

    Added getRandomInt function for generating random numbers and completed the for lopp in line 39

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Assignment Code
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+} 
 var generateBtn = document.querySelector("#generate");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = lowerCase.toUpperCase();
@@ -31,9 +34,11 @@ if (includeNumeric == "y") {
 if (includeSpecial == "y") {
   passwordChoice = passwordChoice.concat(special);
 }
+var generatedPassword = ""
+
 for (let index = 0; index < passwordLength; index++) {
-  const element = array[index];
-  
+  var characterIndex = getRandomInt(passwordChoice.length);
+  passwordChoice[characterIndex];  
 }
 }
 
